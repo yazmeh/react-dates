a9702d581307ca95adeca342c6decabda774551c
Yash Mehata <yazmeh.tech@gmail.com>
2018-05-23 17:50:00 +0530

    onCancel fixed

--- src/components/DateTimePickerComponent.jsx
+++ src/components/DateTimePickerComponent.jsx
@@ -141,7 +141,11 @@ export default class DateTimePickerComponent extends React.Component {
   onCancel() {
     const { selected: { startDate, endDate } } = this.state;
     const { onCancel } = this.props;
-    if (onCancel){
+    this.setState({
+      startDate,
+      endDate,
+    });
+    if(onCancel){
       onCancel({ startDate, endDate });
     }
     this.onFocusChange(null);
