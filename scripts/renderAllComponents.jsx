77392ae0284b9d7bd83071ab54467c814e362f00
yash.m <yash.m@media.net>
2018-06-08 18:49:36 +0530

    Added DateRangeComponent

--- scripts/renderAllComponents.jsx
+++ scripts/renderAllComponents.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DateTimePickerComponent from '../src/components/DateTimePickerComponent';
+import DatePickerComponent from '../src/components/DatePickerComponent';
 import DateTimeRangePickerWrapper from '../examples/DateTimeRangePickerWrapper';
 import DateRangePickerWrapper from '../examples/DateRangePickerWrapper';
 import SingleDatePickerWrapper from '../examples/SingleDatePickerWrapper';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div>
       <DateTimePickerComponent autoFocus />
+      <DatePickerComponent autoFocus />
       <DateTimeRangePickerWrapper autoFocus />
       <DateRangePickerWrapper autoFocus />
       <SingleDatePickerWrapper autoFocus />
