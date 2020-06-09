8803996853234458696da9ecefe1ca2e9116fb5e
Yash Mehata <yash.m@media.net>
2020-06-09 15:58:43 +0530

    IE fix in single date picker

--- src/components/SingleDateTimePicker.jsx
+++ src/components/SingleDateTimePicker.jsx
@@ -245,9 +245,36 @@ class SingleDateTimePicker extends React.PureComponent {
             showKeyboardShortcuts: false,
         });
     }
-
+    detectIE() {
+        const ua = window.navigator.userAgent;
+    
+        const msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+    
+        const trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            const rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+    
+        const edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+          // Edge (IE 12+) => return version number
+          return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+    
+        // other browser
+        return false;
+    }
     onFocusOut(e) {
         const { onFocusChange } = this.props;
+        if(!!this.detectIE()){
+            if (this.container.contains(e.target)) return;
+        }
         if (this.container.contains(e.relatedTarget || e.target)) return;
         onFocusChange({ focused: false });
     }
